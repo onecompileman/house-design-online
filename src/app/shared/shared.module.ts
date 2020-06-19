e43e348e6b49a1abf44181d6cb009d956228988b
Stephen Vinuya <stephen.vinuya@alphazetta.ai>
2020-06-19 09:10:28 +0800

    fix: bugs

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AddHouseItemComponent } from './modals/add-house-item/add-house-item.component';
+import { IonicModule } from '@ionic/angular';
+import { AvatarOptionComponent } from './components/avatar-option/avatar-option.component';
 
 @NgModule({
-  declarations: [AddHouseItemComponent],
-  imports: [BrowserModule, CommonModule],
-  entryComponents: [AddHouseItemComponent],
+  declarations: [AddHouseItemComponent, AvatarOptionComponent],
+  imports: [BrowserModule, CommonModule, IonicModule],
+  exports: [AddHouseItemComponent],
+  entryComponents: [AddHouseItemComponent, AvatarOptionComponent],
 })
 export class SharedModule {}
