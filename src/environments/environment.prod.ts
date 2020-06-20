2eab657367a5f759dfad9dfe37408969f648bf82
Stephen Vinuya <stephen.vinuya@alphazetta.ai>
2020-06-20 20:17:36 +0800

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyC9pVthg2XqCtYn6MO7xewQLAlZgthGlzU',
-    authDomain: 'myroom-designer.firebaseapp.com',
-    databaseURL: 'https://myroom-designer.firebaseio.com',
-    projectId: 'myroom-designer',
-    storageBucket: 'myroom-designer.appspot.com',
-    messagingSenderId: '705702366624',
-    appId: '1:705702366624:web:03bf00f471041adc46a8c5',
-    measurementId: 'G-6KJ8R4S12W',
-  },
 };
